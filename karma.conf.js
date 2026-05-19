dd88589c1368f5b3dc8407884c163267a5e06513
Michael Johanson <mikejohanson@gmail.com>
2026-05-19 15:03:10 -0700

    refactor: ran prettier

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,8 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        { type: 'lcov' }]
+        { type: 'lcov' }
+      ]
     },
     reporters: [
       'progress',
